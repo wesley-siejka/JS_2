4354ea084eda2a47e36d96187ed8362ee2c34c10
wesley-siejka <126927077+wesley-siejka@users.noreply.github.com>
2023-10-10 11:39:05 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -1,15 +1,38 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i <pacientes.length ; i++){
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var paciente = pacientes[i];
 
-var tdImc = paciente.querySelector(".info-imc");
-var imc = peso / (altura * altura);
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    
+    var pessoa_valida = true
+    
+    if (peso <= 0 || peso >= 400){
+        console.log("Pessoa invalida");
+        pessoa_valida = false
+        tdImc.textContent = "Pessoa invalida"
+    }
+    
+    if (altura <= 0 || altura >= 3.5){
+        console.log("Pessoa invalida");
+        pessoa_valida = false
+        tdImc.textContent = "Pessoa invalida"
+    }
+    
+    if (pessoa_valida){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2); 
+    }
+}
 
-tdImc.textContent = imc
